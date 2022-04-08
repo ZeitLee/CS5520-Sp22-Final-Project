24e0570aff7e7159c969ad7a3298af19610fdc3e
ybai5github <ybai5@binghamton.edu>
2022-04-08 13:31:46 -0400

    Create reminder UI

--- app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/createReminder.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/createReminder.java
@@ -5,21 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class createReminder extends AppCompatActivity {
+
+    EditText nameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_reminder);
+
+        nameInput = (EditText) findViewById(R.id.editTextTaskName);
     }
 
-    public void newReminder(View V){//view occupies a rectangular area on the screen and
+    public void backtoMain(View V){//view occupies a rectangular area on the screen and
         // is responsible for drawing and event handling
         //Toast.makeText(getApplicationContext(), "wiggle wiggle", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, createReminder.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 
 }
