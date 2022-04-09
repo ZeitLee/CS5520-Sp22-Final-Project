d694f20afdc7be99127f043babc6437f0e4baee0
ZeitLee <li.zes@husky.neu.edu>
2022-04-08 20:18:58 -0400

    add calendar date selector.

--- app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void newReminder(View V){//view occupies a rectangular area on the screen and
+    public void newReminder(View V){
+        //view occupies a rectangular area on the screen and
         // is responsible for drawing and event handling
         //Toast.makeText(getApplicationContext(), "wiggle wiggle", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, createReminder.class);
--- app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/createReminder.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/createReminder.java
@@ -2,14 +2,26 @@ package edu.neu.madcourse.cs5520_sp22_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class createReminder extends AppCompatActivity {
 
-    EditText nameInput;
+    private EditText nameInput;
+    private TextView myTextDisplayDate;
+    private ImageView myImageDisplayDate;
+    private DatePickerDialog.OnDateSetListener myDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +29,58 @@ public class createReminder extends AppCompatActivity {
         setContentView(R.layout.activity_create_reminder);
 
         nameInput = (EditText) findViewById(R.id.editTextTaskName);
+        myTextDisplayDate = (TextView) findViewById(R.id.dateSelector);
+        myImageDisplayDate = (ImageView) findViewById(R.id.dataImageView);
+
+        // set click listener to date text and image to open date setting dialog.
+        myTextDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateSelecotr();
+            }
+        });
+
+        myImageDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateSelecotr();
+            }
+        });
+
+        // set date picker listener.
+        myDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                int showMonth = month + 1; // since the month is 0-based data.
+                String date = String.format("%s/%s/%s", showMonth, day, year);
+                myTextDisplayDate.setText(date);
+            }
+        };
     }
 
-    public void backtoMain(View V){//view occupies a rectangular area on the screen and
+    public void backtoMain(View V){
+        //view occupies a rectangular area on the screen and
         // is responsible for drawing and event handling
         //Toast.makeText(getApplicationContext(), "wiggle wiggle", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    // This is a helper method to show date selector screen.
+    private void showDateSelecotr() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day  = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(createReminder.this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth, myDateSetListener,
+                year, month, day);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+
 
 }
