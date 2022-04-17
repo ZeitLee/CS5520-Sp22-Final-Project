8208c8770c953165e8d10fccbd5aaf240a8e71d9
ZeitLee <li.zes@husky.neu.edu>
2022-04-17 17:52:24 -0400

    merge main screen and create screen.

--- app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_sp22_final_project/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         addIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createNewReminder();
+                newReminder(view);
             }
         });
 
@@ -121,7 +121,6 @@ public class MainActivity extends AppCompatActivity {
                 itemList.add(obj);
             }
         }
-
     }
 
     private void createRecyclerView() {
